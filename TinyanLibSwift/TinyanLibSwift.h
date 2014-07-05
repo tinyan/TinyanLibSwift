b8bd4a4804ed4cec7ce8067d89993b0ac4a16ac1
Tinyan <tinyan@mri.biglobe.ne.jp>
2014-07-05 21:43:32 +0900

    Initial Commit

---
+++ TinyanLibSwift/TinyanLibSwift.h
@@ -0,0 +1,19 @@
+//
+//  TinyanLibSwift.h
+//  TinyanLibSwift
+//
+//  Created by たいにゃん on 2014/07/05.
+//  Copyright (c) 2014年 bugnekosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyanLibSwift.
+FOUNDATION_EXPORT double TinyanLibSwiftVersionNumber;
+
+//! Project version string for TinyanLibSwift.
+FOUNDATION_EXPORT const unsigned char TinyanLibSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyanLibSwift/PublicHeader.h>
+
+
